fe327cacf0715795dc7e98efa77297b7621fc599
GBTC405007UR <GBTC405007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-20 11:52:12 -0400

    202 exercise

---
+++ src/main/java/com/example/demo/SpringBoot202exApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot202exApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot202exApplication.class, args);
+    }
+
+}
